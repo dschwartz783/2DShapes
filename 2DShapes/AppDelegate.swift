c14fbd5ec0cf86cea3a6975dac35137be564b605
dschwartz783 <dschwartz783@gmail.com>
2017-01-29 20:54:48 -0500

    Initial Commit

---
+++ 2DShapes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  2DShapes
+//
+//  Created by David Schwartz on 1/29/17.
+//  Copyright © 2017 DDS Programming. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 2DShapes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  2DShapes
+//
+//  Created by David Schwartz on 1/29/17.
+//  Copyright © 2017 DDS Programming. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
